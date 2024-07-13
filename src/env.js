cbb3dfae0657fe08a237c6739777dda200323eaa
Md Irshad Ansari <irshadjsr21@gmail.com>
2024-07-13 14:57:54 +0530

    build: enabled nextjs static export

--- src/env.js
+++ src/env.js
@@ -1,6 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
